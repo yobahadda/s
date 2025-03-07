a0ffff8a38fcf11780a8eb6f4f58039f41408c99
bahaddaaayoub <bahaddaayoub123@gmail.com>
2025-03-07 01:49:53 +0000

    rak tema

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  // Ensure CSS processing is enabled
+  webpack: (config) => {
+    return config
+  },
+  cssModules: true
 };
 
 export default nextConfig;
